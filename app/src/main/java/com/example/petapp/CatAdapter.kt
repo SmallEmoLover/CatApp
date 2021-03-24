b5c2d79634308577780f7678fb3ee3fe3a0cdf1f
SmallEmoLover <missingtext@yandex.ru>
2021-03-24 15:15:04 +0300

    Created layout

--- app/src/main/java/com/example/petapp/CatAdapter.kt
+++ app/src/main/java/com/example/petapp/CatAdapter.kt
@@ -20,13 +20,19 @@ class CatAdapter(private val values: List<Cat>) : RecyclerView.Adapter<CatAdapte
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val cat = values[position]
-        holder.itemName?.text = cat.name
+        holder.catName?.text = cat.name
+        holder.catKind?.text = cat.kind
+        holder.catAge?.text = "${cat.age} years"
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var itemName: TextView? = null
+        var catName: TextView? = null
+        var catAge: TextView? = null
+        var catKind: TextView? = null
         init {
-            itemName = itemView.findViewById(R.id.cat_name)
+            catName = itemView.findViewById(R.id.cat_name)
+            catAge = itemView.findViewById(R.id.cat_age)
+            catKind = itemView.findViewById(R.id.cat_kind)
         }
     }
 }
