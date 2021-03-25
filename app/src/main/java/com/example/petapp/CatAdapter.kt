8503fbf82ce9d655ee7fa98e4468cda18cf0a677
SmallEmoLover <missingtext@yandex.ru>
2021-03-25 11:29:47 +0300

    Created extension function

--- app/src/main/java/com/example/petapp/CatAdapter.kt
+++ app/src/main/java/com/example/petapp/CatAdapter.kt
@@ -10,17 +10,18 @@ import com.example.petapp.model.Cat
 
 
 class CatAdapter(
-        private val values: List<Cat>,
-        private val onClick: (Cat) -> Unit
+    private val values: List<Cat>,
+    private val onClick: (Cat) -> Unit
 ) : RecyclerView.Adapter<CatAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.cat_list_item, parent, false)
-        return ViewHolder(itemView)
+    fun ViewGroup.createViewHolder(layoutId: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(this.context).inflate(layoutId, this, false))
     }
 
-    override fun getItemCount(): Int = values.size
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        parent.createViewHolder(R.layout.cat_list_item)
+
+    override fun getItemCount() = values.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val cat = values[position]
@@ -28,13 +29,14 @@ class CatAdapter(
         holder.catKind?.text = cat.kind
         holder.catAge?.text = "${cat.age} years"
 
-        holder.itemView.setOnClickListener() { onClick.invoke(cat) }
+        holder.itemView.setOnClickListener { onClick.invoke(cat) }
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var catName: TextView? = null
         var catAge: TextView? = null
         var catKind: TextView? = null
+
         init {
             catName = itemView.findViewById(R.id.cat_name)
             catAge = itemView.findViewById(R.id.cat_age)
